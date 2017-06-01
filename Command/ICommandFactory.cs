d93d4339efa7ec7663757e8db3c3b959fab555d3
sebas77 <ssense@tiscali.it>
2017-06-01 15:29:34 +0100

    delete files added by mistake

--- Command/ICommandFactory.cs
+++
@@ -1,8 +0,0 @@
-namespace Svelto.Command
-{
-    interface ICommandFactory
-    {
-        TCommand Build<TCommand>() where TCommand : ICommand, new();
-    }
-}
-
--- IoC/IInitialize.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Svelto.IoC
-{
-    interface IInitialize
-	{
-		void OnDependenciesInjected();
-	}
-}
